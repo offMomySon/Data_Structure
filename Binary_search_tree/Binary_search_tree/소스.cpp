292067c23fb18c358a3061a3b0b0cd6b5e1e381f
오지훈 <ohji1006@knu.ac.kr>
2018-09-24 04:46:37 +0900

    Full version1

--- Binary_search_tree/Binary_search_tree/소스.cpp
+++ Binary_search_tree/Binary_search_tree/소스.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<Windows.h>
 
 typedef struct Node * NodePtr;
 typedef struct Node
@@ -13,15 +14,68 @@ typedef struct Node
 
 NodePtr start_point;
 
+void inorder(NodePtr tree);
+NodePtr make_node(int value);
+void insert_node(NodePtr * tree, NodePtr new_node);
+NodePtr search_node(NodePtr tree, int find_value);
+void delete_node(NodePtr *tree, int delete_value);
+int find_min_arm(NodePtr* tree);
 
 
 int main()
 {
+	printf("Enter num(10개) : ");
 
+	for (int i = 0; i < 10; i++)
+	{
+		int num;
+		scanf("%d", &num);
 
+		insert_node(&start_point, make_node(num));
+	}
 
+	printf("Entered Num is : ");
+	inorder(start_point);
+	printf("\n");
+
+	for (int i = 0; i < 5; i++)
+	{
+		int num;
+		printf("Enter search num : ");
+		scanf("%d", &num);
+
+		NodePtr result_ptr = search_node(start_point, num);
+		if (NULL == result_ptr)
+			printf("Can not Find %d\n", num);
+		else
+			printf("%d is detected in address : %#x \n", num, result_ptr);
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		int num;
+		printf("Delete num : ");
+		scanf("%d", &num);
+
+		delete_node(&start_point, num);
+
+		inorder(start_point);
+		printf("\n");
+	}
+
+	system("pause");
 }
 
+
+void inorder(NodePtr tree)
+{	
+	if (tree != NULL)
+	{
+		inorder(tree->l_arm);
+		printf("[%d] ", tree->value);
+		inorder(tree->r_arm);
+	}
+}
 
 NodePtr make_node(int value)
 {
@@ -54,8 +108,10 @@ void insert_node(NodePtr * tree, NodePtr new_node)
 
 NodePtr search_node(NodePtr tree, int find_value)
 {
-	NodePtr return_ptr;
-
+	NodePtr return_ptr = NULL;
+	if (tree == NULL)
+		return return_ptr;
+	
 	if (tree->value == find_value)
 		return_ptr = tree;
 
@@ -73,7 +129,8 @@ void delete_node(NodePtr *tree, int delete_value)
 	{
 		if ((*tree)->l_arm == NULL && (*tree)->r_arm == NULL)   // subtree없다.
 		{
-			free((*tree));  // 그냥 제거
+			free((*tree));  // 그냥 제거-
+			(*tree) = NULL;
 		}
 		else if (((*tree)->l_arm == NULL && (*tree)->r_arm != NULL) || ((*tree)->r_arm == NULL && (*tree)->l_arm != NULL))  //둘중하나 subtree 존재
 		{
@@ -84,13 +141,10 @@ void delete_node(NodePtr *tree, int delete_value)
 		}
 		else if ((*tree)->l_arm != NULL && (*tree)->r_arm != NULL) // 양쪽다 subtree 있다.
 		{
-			NodePtr temp_ptr = find_min_arm( &((*tree)->r_arm ));
-			temp_ptr->l_arm = (*tree)->l_arm;
-			temp_ptr->r_arm = (*tree)->r_arm;
-
-			free(*tree);
-
-			(*tree) = temp_ptr;
+			int temp_value = find_min_arm( &((*tree)->r_arm )); // 오른쪽 팔을 출발 시점으로 지정 // return 값은 왼쪽으로 더이상 갈 수 없는 노드.
+			
+			(*tree)->value = temp_value;
+	
 		}
 		
 	}
@@ -101,20 +155,22 @@ void delete_node(NodePtr *tree, int delete_value)
 }
 
 
-
-NodePtr find_min_arm(NodePtr* tree)
+int find_min_arm(NodePtr* tree)
 {
-	if ((*tree)->l_arm == NULL)
+	if ((*tree)->l_arm == NULL)   // 왼쪽으로 더이상 갈 수 없다.
 	{
-		NodePtr result_ptr = *tree;
-
-		if ((*tree)->r_arm != NULL)
+		int rtn_value = (*tree)->value;
+		NodePtr temp = (*tree);
+		if ((*tree)->r_arm != NULL)    //지금의 node에서 r_arm에 값이 있으면 포인터를 대체 해야함. 
 			(*tree) = (*tree)->r_arm;
+		else
+			(*tree) = NULL;
+		free(temp);
 		
-		return result_ptr;
+		return rtn_value;
 	}
 	else
-		return find_min_arm( &((*tree)->l_arm) );
+		return find_min_arm( &((*tree)->l_arm) );  // 왼쪽으로 계속이동
 }
 
 
